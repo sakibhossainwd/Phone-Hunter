b50dc9206f2a29bafa0788ca1fce8dc691949965
sakibhossain <sakibhossainceo@gmail.com>
2023-06-20 13:54:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// async await
+const loadPones = async() => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=samsung`
+    // main async await steps
+
+    // const res = await fetch(url);
+    // const data = await res.json();
+    // displayMealDetails(data)
+
+    // add error and async await
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayPhonesDetails(data.data)
+    }
+    catch(error){
+        console.log('Error:- ', error)
+    }
+};
+
+const displayPhonesDetails = phones => {
+    console.log(phones);
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add("card");
+        div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                  <button class="btn btn-warning">See Details</button>
+                </div>
+        `
+        phonesContainer.appendChild(div);
+    });
+}
+
+
+loadPones();
